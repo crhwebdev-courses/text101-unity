067eca3c35131e9c9ebf4a4a68a9a3e24ee5472a
Carl Hain <crhain@gmail.com>
2020-07-12 13:26:23 -0500

    Refactor AdventureGame script to be more reusable for future story states

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -6,10 +6,14 @@ public class AdventureGame : MonoBehaviour
     [SerializeField] private Text textComponent;
     [SerializeField] private State startingState;
 
+    State state;
+
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = startingState.GetStateStory();
+        
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
