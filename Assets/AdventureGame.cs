492fbc837aa63bb347bc9c5c6871e00940400ff0
Carl Hain <crhain@gmail.com>
2020-07-12 13:19:38 -0500

    Hook up StartState scriptable object to Game object and display text using Adventure script

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -4,11 +4,12 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] private Text textComponent;
+    [SerializeField] private State startingState;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Hello World!";
+        textComponent.text = startingState.GetStateStory();
     }
 
     // Update is called once per frame
--- Assets/State.cs
+++ Assets/State.cs
@@ -3,5 +3,10 @@
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-   [TextArea(10, 14)] [SerializeField] private string storyText;
+    [TextArea(10, 14)] [SerializeField] private string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
 }
